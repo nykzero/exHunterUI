2a91f4fe832fb040fe7af50e89682e0660da37e3
Nick Clifton <nicholas.clifton@gmail.com>
2017-08-01 17:24:50 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SearchComponent } from './search/search.component';
+import { QuestComponent } from './quest/quest.component';
+import { RankingComponent } from './ranking/ranking.component';
+import { CONST_ROUTING } from './app.routing'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ProfileComponent,
+    SearchComponent,
+    QuestComponent,
+    RankingComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    CONST_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
